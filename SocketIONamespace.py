72af4a33a4a6af45ddef27f9202a6979aef7f0e7
spdconvos <76911550+spdconvos@users.noreply.github.com>
2021-04-19 17:12:21 -0700

    Correctly register handler

--- SocketIONamespace.py
+++ SocketIONamespace.py
@@ -11,29 +11,30 @@ class SocketIONamespace(socketio.ClientNamespace):
         super().__init__(namespace=namespace)
 
     def on_connect(self):
-        """ config = {
+        config = {
             "filterCode": [44912, 45040, 45112, 45072, 45136],
             "filterType": "talkgroup",
             "filterName": "OpenMHZ",
             "filterStarred": False,
             "shortName": "kcers1b",
-        } """
-        # DEBUG CONFIG TO GET A LOT OF API RESPONSES
+        }
+        """ # DEBUG CONFIG TO GET A LOT OF API RESPONSES
         config = {
-            "filterCode": "",
-            "filterType": "all",
+            "filterCode": "5ed813629818fe0025c8e245",
+            "filterType": "group",
             "filterName": "OpenMHZ",
             "filterStarred": False,
             "shortName": "kcers1b",
-        }
+        } """
         self.emit("start", config)
+        self.client.on("new message", self.handleMessage)
 
     def on_disconnect(self):
         log.info("Disconnected")
 
-    def on_new_message(self, data):
-        json = json.loads(data)
-        self.parent.postTweet(json)
+    def handleMessage(self, data):
+        jsonData = json.loads(data)
+        self.parent.postTweet(jsonData)
 
         if self.parent.reportLatency:
             sum = sum(self.parent.latency).total_seconds()
